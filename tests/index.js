98fa2b0a26635f68096ece7ba28c0e733aa81227
sethvincent <sethvincent@gmail.com>
2016-03-25 15:19:35 -0700

    reorganize tests, use testron, rewrite npm scripts

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,22 @@
 var test = require('tape')
+var request = require('../request')
 var gh = require('../index')({ token: process.env.GITHUB_ACCESS_TOKEN })
+
+test('request an organization', function (t) {
+  t.plan(3)
+
+  var options = {
+    endpoint: 'orgs/editdata',
+    token: process.env.GITHUB_ACCESS_TOKEN,
+    json: true
+  }
+
+  request(options, function (err, res, body) {
+    t.notOk(err)
+    t.ok(res)
+    t.ok(body)
+  })
+})
 
 test('request contents of a repo', function (t) {
   t.plan(4)
